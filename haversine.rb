92dec82067c9529d1013201fc4cda842cd16c730
almartin <alex.m.martineau@gmail.com>
2011-03-14 21:06:30 -0400

    Placed code into a proper function

--- haversine.rb
+++ haversine.rb
@@ -3,38 +3,46 @@
 #
 # Author: almartin <alex.m.martineau@gmail.com>
 # Language: Ruby
-# Descrption: Example script implementing Haversine formula for use in geolocation
+# Descrption: Example script implementing Haversine formula to identify distance in KM between 2 coordinates
+# License: See LICENSE file
 #
 
-earthRadius = 6371 # Earth's radiusin KM
+def coorDist(lat1, lon1, lat2, lon2)
 
+    earthRadius = 6371 # Earth's radius in KM
 
-# hardcoded example coordinates #
-lat1 = 41.5592880250001
-lat2 = 41.8574079970001
-lon1 = -73.0371704099999
-lon2 = -72.00779572399991
-#################################
+        # convert degrees to radians
+        def convDegRad(value)
+          unless value.nil? or value == 0
+                value = (value/180) * Math::PI
+          end
+        return value
+        end
 
-    def convDegRad(value)
-      unless value.nil? or value == 0
-            value = (value/180) * Math::PI
-      end
-    return value
-    end
-
-    deltaLat = (lat2-lat1) # Lat prior to conversion to radian
-    deltaLon = (lon2-lon1) # lon prior to conversion to radian
+    deltaLat = (lat2-lat1)
+    deltaLon = (lon2-lon1)
     deltaLat = convDegRad(deltaLat)
     deltaLon = convDegRad(deltaLon)
 
     # Calculate square of half the chord length between latitude and longitude
     a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-        Math.cos((lat1/180 * Math::PI)) * Math.cos((lat2/180 * Math::PI)) * 
+        Math.cos((lat1/180 * Math::PI)) * Math.cos((lat2/180 * Math::PI)) *
         Math.sin(deltaLon/2) * Math.sin(deltaLon/2); 
 
     # Calculate the angular distance in radians
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
 
     distance = earthRadius * c
-    puts distance
+
+  return distance
+
+end
+
+    # hardcoded example coordinates #
+    lat1 = 41.5592880250001
+    lat2 = 41.8574079970001
+    lon1 = -73.0371704099999
+    lon2 = -72.00779572399991
+    #################################
+
+    puts coorDist(lat1, lon1, lat2, lon2)
